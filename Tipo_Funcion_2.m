713e77858f0661eec05a3dec767ab2b5b59f772b
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-11-16 19:05:26 -0600

    Función 2
    Función 2

---
+++ Tipo_Funcion_2.m
@@ -0,0 +1,25 @@
+% Octave Script
+% Title                      :Tipo_Funcion_2
+% Description                :Script para saber que tipo de ecuacion se forma, suprayectiva, biyectiva o inyectiiva.
+% Author                     :Gustavo García Barrera(NONE_NAME)
+% Date                       :20211116
+% Version                    :1     
+% Usage                      :Este equipo> Documentos >Scripts Calculo Dif acripts calculod> Resolver_Inecuaciones
+%                            :Requiere aplicación octave para usar su línea de comandos
+%                            :https://octaveintro.readthedocs.io/en/latest/index.html
+
+% Limpiar variables
+clear
+% Inicio de paquetería symbolic
+syms x
+% Dominio
+x=-15:1:20;
+% Funcion a plotear
+fx= x.^3
+% Dibujar x, fx
+plot (x, fx);
+hold on 
+grid on;
+% Título
+title(['Funcion 2'])
+disp('Función biyectiva');
